d96cf9e4fcb921543526af2be389928ea0488c3b
Francis <33382333+umbrella487@users.noreply.github.com>
2023-01-02 22:08:19 +0000

    Update __init__.py

--- vectordataalgorithm/__init__.py
+++ vectordataalgorithm/__init__.py
@@ -1,15 +1,4 @@
-# When the pyodide package is imported, both the js and the pyodide_js modules
-# will be available to import from. Not all functions in pyodide_js will work
-# until after pyodide is first imported, imported functions from pyodide_js
-# should not be used at import time. It is fine to use js functions at import
-# time.
-#
-# All pure Python code that does not require js or pyodide_js should go in
-# the _pyodide package.
-#
-# This package is imported by the test suite as well, and currently we don't use
-# pytest mocks for js or pyodide_js, so make sure to test "if IN_BROWSER" before
-# importing from these.
+#Vector Data algorithm for standalone GIS projects
 __version__ = "1.0.0"
 
 from .geometry import Point 
